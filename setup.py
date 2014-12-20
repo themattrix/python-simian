018c555b99e031045cfa5c64a33a044284efceee
Matthew Tardiff <mattrix@gmail.com>
2014-12-20 13:25:57 -0600

    Added flake8 to TravisCI.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Matthew Tardiff',
     author_email='mattrix@gmail.com',
     install_requires=('mock',),
-    tests_require=('nose',),
+    tests_require=('nose', 'flake8'),
     description=(
         'A decorator for easily mocking out multiple dependencies by '
         'monkey-patching.'))
