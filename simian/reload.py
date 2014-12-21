4606c2a7a88c452c92eab98e78c6bcf446614b6a
Matthew Tardiff <mattrix@gmail.com>
2014-12-21 09:34:21 -0600

    Ignoring unused import reload.

--- simian/reload.py
+++ simian/reload.py
@@ -1,10 +1,10 @@
 try:
     # noinspection PyUnresolvedReferences
-    from importlib import reload
+    from importlib import reload  # NOQA
 except ImportError:  # pragma: no cover
     try:  # pragma: no cover
         # noinspection PyUnresolvedReferences
-        from imp import reload  # pragma: no cover
+        from imp import reload  # NOQA pragma: no cover
     except ImportError:  # pragma: no cover
         # Fall back to the built-in "reload", which should be present when not
         # contained in importlib or imp.
