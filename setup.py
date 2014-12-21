b45dfe108a9a0fba0dc3e9be4e2f927a6e32b96b
Matthew Tardiff <mattrix@gmail.com>
2014-12-21 17:43:23 -0600

    Let's see if we can push to PyPI from a containerized build.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simian',
-    version='1.0.0',
+    version='1.0.1',
     packages=('simian',),
     url='https://github.com/themattrix/python-simian',
     license='MIT',
